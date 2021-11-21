59c1400021db32aba6f324bee2ccaffcbde94d99
Kyoo130 <kml9003@kakao.com>
2021-11-21 22:51:27 +0900

    [update] move scroll add commit

---
+++ InteractiveWeb/3dscroll/js/wall3d.js
@@ -0,0 +1,16 @@
+(function () {
+  const houseElem = document.querySelector(".house");
+  let maxScrollValue;
+
+  function resizeHandler() {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  }
+
+  window.addEventListener("scroll", function () {
+    const zMove = (pageYOffset / maxScrollValue) * 980 - 490;
+    houseElem.style.transform = "translateZ(" + zMove + "vw)";
+  });
+
+  window.addEventListener("resize", resizeHandler);
+  resizeHandler();
+})();
